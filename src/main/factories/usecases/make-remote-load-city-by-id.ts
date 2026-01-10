78e45e1c430faacf39365c37f93e9240142d23ed
Leonardo Cardoso de Oliveira <leocardosodev@gmail.com>
2026-01-10 19:31:25 -0300

    feat: add remote city lookup factory
    Ref task-009-05

---
+++ src/main/factories/usecases/make-remote-load-city-by-id.ts
@@ -0,0 +1,7 @@
+import { RemoteLoadCityById } from '@/data/usecases/remote-load-city-by-id'
+import { LoadCityById } from '@/domain/usecases/load-city-by-id'
+import { makeHttpClient } from '@/main/factories/http/api-client-factory'
+
+export const makeRemoteLoadCityById = (): LoadCityById => {
+  return new RemoteLoadCityById('/cities', makeHttpClient())
+}
