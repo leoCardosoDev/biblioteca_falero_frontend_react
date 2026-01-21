0f22f5053304265c5ceb73bd84980f902ecb1ab3
Leonardo Cardoso <leocardosodev@gmail.com>
2026-01-20 21:20:08 -0300

    build: update tsconfig and dashboard structure tests

--- src/shared/domain/constants/messages.ts
+++ src/shared/domain/constants/messages.ts
@@ -1,3 +1,8 @@
 export const DomainErrorMessages = {
   UnexpectedTryAgain: 'Erro inesperado. Tente novamente mais tarde.'
 } as const
+
+export const ErrorMessages = {
+  UnexpectedReload:
+    'Ocorreu um erro inesperado. Por favor, recarregue a página.'
+} as const
